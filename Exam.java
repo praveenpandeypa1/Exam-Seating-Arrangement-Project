2003369c48aca37e7f8bd5e9afdb5c31423561e0
praveenpandeypa1 <125757589+praveenpandeypa1@users.noreply.github.com>
2023-03-10 13:14:17 +0530

    Update Exam.java

--- Exam.java
+++ Exam.java
@@ -7,3 +7,25 @@ import org.hibernate.cfg.AnnotationConfiguration;
 
 import com.pojo.ExamDetails;
 
+
+public class Exam {
+public boolean AddExam(ExamDetails examDetails) {
+		
+		AnnotationConfiguration cfg=new AnnotationConfiguration();
+		cfg.configure("com/configure/ExamDetails.cfg.xml");
+		SessionFactory sf=cfg.buildSessionFactory();
+		
+		Session session=sf.openSession();
+		
+		Transaction tx=session.beginTransaction();
+		
+		session.save(examDetails);
+		tx.commit();
+		
+		
+		return true;
+
+}
+
+}
+
