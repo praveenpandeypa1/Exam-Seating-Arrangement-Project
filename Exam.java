88641eb90142b6d46d195bab411f5760348dd3e2
praveenpandeypa1 <125757589+praveenpandeypa1@users.noreply.github.com>
2023-03-10 13:14:03 +0530

    Create Exam.java

---
+++ Exam.java
@@ -0,0 +1,9 @@
+package com.model;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.AnnotationConfiguration;
+
+import com.pojo.ExamDetails;
+
