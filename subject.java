f67be7584d34b1677d38d30a35db9b8a91f4ef28
Praveen kumar pandey <125757589+praveenpandeypa1@users.noreply.github.com>
2023-03-11 17:49:21 +0530

    Update subject.java

--- subject.java
+++ subject.java
@@ -16,3 +16,27 @@ public boolean AddSubject(SubjectDetails subjectDetails) {
 		
 		return true;
 	}
+	public boolean UpdateSubject(SubjectDetails subjectDetails) {
+		
+		AnnotationConfiguration cfg=new AnnotationConfiguration();
+		cfg.configure("com/configure/SubjectDetails.cfg.xml");
+		SessionFactory sf=cfg.buildSessionFactory();
+		
+		Session session=sf.openSession();
+		Transaction tx=session.beginTransaction();  
+		Query q=session.createQuery("update SubjectDetails set subjectname=:v, subjectcode=:c, branch=:b, sem=:s where id=:i");  
+		q.setParameter("v",subjectDetails.getSubjectname());  
+		q.setParameter("c",subjectDetails.getSubjectcode());  
+		q.setParameter("b",subjectDetails.getBranch()); 
+		q.setParameter("s",subjectDetails.getSem());  
+		q.setParameter("i",subjectDetails.getId()); 
+		  
+		int status=q.executeUpdate();  
+		System.out.println(status);  
+		tx.commit();
+		
+		
+		return true;
+	}
+}
+
