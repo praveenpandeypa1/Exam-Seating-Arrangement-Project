b4aa376518a4c0262ae6644f6f2e7b3fb28034d7
Praveen kumar pandey <125757589+praveenpandeypa1@users.noreply.github.com>
2023-03-13 16:38:17 +0530

    Update teacher.java

--- teacher.java
+++ teacher.java
@@ -12,3 +12,16 @@ public class Teacher {
 		
 		return true;
 	}
+	public List TeacherDetails(TeacherDetails teacherDetails) {
+		
+		AnnotationConfiguration cfg = new AnnotationConfiguration();
+		cfg.configure("com/configure/TeacherDetails.cfg.xml");
+		SessionFactory sessionFactory = cfg.buildSessionFactory();
+		Session session = sessionFactory.openSession();
+		Transaction transaction = session.beginTransaction();
+		
+		Criteria criteria = session.createCriteria(TeacherDetails.class);
+		criteria.add(Restrictions.eq("id", teacherDetails.getId()));
+		List teacher=criteria.list();
+		transaction.commit();
+		return teacher;
